d9aa7ccc47ce2df986e93154704d8ea08579140b
Jeff Tranter <tranter@pobox.com>
2019-01-26 13:29:44 -0500

    Add a simple example to test loading from monitor.

---
+++ sbc/examples/ex1.asm
@@ -0,0 +1,13 @@
+; Simple 6809 assembler example.
+; Does not do anything meaningful, but can be used for testing file
+; loading and running.
+
+        ORG     $7000           ; Start address
+
+START:  LDA     #$12
+        LDB     #$34
+        LDX     #$5678
+        EXG     A,B
+        NOP
+        NOP
+        RTS
