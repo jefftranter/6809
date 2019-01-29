389342583a0fd456ceb5929c813a9f573c985fb2
Jeff Tranter <tranter@pobox.com>
2019-01-29 17:51:25 -0500

    Working input example.

--- sbc/cmoc/ex2.c
+++ sbc/cmoc/ex2.c
@@ -20,18 +20,38 @@ done:   puls    b,x     // Restore registers
     }
 }
 
+// Read a character from the console and return it.
+char readchar()
+{
+    char c;
+
+    asm
+    {
+loop:   lda     $A000   // Read ACIA status register
+        lsra            // Shift RDR bit into carry
+        bcc     loop    // Branch if not character yet
+        lda     $A001   // Read data register
+        sta     :c      // Store in variable c
+    }
+
+    return c;
+}
+
+static char linebuf[80]; // Buffer for readline
+
 // Replacement for readline library function.
 // Reads a line from standard input and returns it.
-// Not thread-safe.
-// Returns a null pointer if the operation failed (e.g., end of file
-// encountered).
-//
-
-static char linebuf[80];
-
 char *readline()
 {
-    strncpy(linebuf, "entered line", sizeof(linebuf));
+    int i;
+
+    for (i = 0; i < sizeof(linebuf); i++) {
+        linebuf[i] = readchar();
+        if (linebuf[i] == '\r' || linebuf[i] == '\r' || linebuf[i] == '\0') {
+            break;
+        }
+    }
+    linebuf[i] = 0;
     return linebuf;
 }
 
