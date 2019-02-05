88db862ec6fecfc0542bc706f495a4394e372118
Jeff Tranter <tranter@pobox.com>
2019-02-05 17:43:30 -0500

    Add example of parsing command line option.

--- sbc/examples/ex3.asm
+++ sbc/examples/ex3.asm
@@ -1,6 +1,6 @@
 ; Example of adding custom command to ASSIST09 using secondary command
 ; list. After calling this code from the monitor, typing the command U
-; will display the string Hello!
+; will get an address from the user and then display it.
 
 ; Character defines
 
@@ -9,9 +9,16 @@ EOT     EQU     $04             ; String terminator
 ; ASSIST09 SWI call numbers
 
 PDATA1  EQU     2               ; Output string
-PCRLF   EQU     6               ; OUTPUT CR/LF
+OUT4HS  EQU     5               ; Output four hex and space
+PCRLF   EQU     6               ; Output CR/LF
 VCTRSW  EQU     9               ; Vector swap
 .CMDL2  EQU     44              ; Secondary command list
+
+; ASSIST09 Routines and data
+
+CDNUM   EQU     $FE60           ; Note: Only valid for my ROM version
+PCNTER  EQU     $6093           ; Stores last PC value
+
 
         ORG     $1000           ; Start address
 
@@ -21,20 +28,39 @@ START   LEAX    MYCMDL,PCR      ; Load new handler address
         FCB    VCTRSW           ; Service code byte
         RTS                     ; Return to monitor
 
+; Command table.
+
 MYCMDL:
         FCB     4               ; Table entry length
         FCC     'U'             ; Command name
         FDB     UCMD-*          ; Pointer to command (relative to here)
         FCB     $FE             ; -2 indicates end of table
 
-UCMD:
+; The actual command.
+
+UCMD:   LBSR    CDNUM           ; Parse command line, return 16-bit number in D
         LEAX    MSG1,PCR        ; Get address of string to display
         SWI                     ; Call ASSIST09 monitor function
         FCB     PDATA1          ; Service code byte
+        TFR     D,X             ; Put entered address in X
+        BSR     PrintAddress    ; Print it
         SWI                     ; Print CR/LF
         FCB     PCRLF
+        RTS                     ; Return to monitor
+
+TEMP    RMB     2               ; Temp variable (used by print routines)
+
+; Print a word as four hex digits followed by a space.
+; X contains word to print.
+; Registers changed: none
+PrintAddress:
+        PSHS    A,B,X           ; Save registers used
+        STX     TEMP            ; Needs to be in memory so we can point to it
+        LEAX    TEMP,PCR        ; Get pointer to it
+        SWI                     ; Call ASSIST09 monitor function
+        FCB     OUT4HS          ; Service code byte
+        PULS    X,B,A           ; Restore registers used
         RTS
 
-MSG1    FCC     'Hello!'
+MSG1    FCC     'Entered: '
         FCB     EOT
-
