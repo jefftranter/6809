ead9bad2043ac3dee3b2b3fe7d34f895aa719c68
Jeff Tranter <tranter@pobox.com>
2019-02-09 15:12:30 -0500

    Use 30K of memory.

--- sbc/examples/ex4.asm
+++ sbc/examples/ex4.asm
@@ -1,7 +1,7 @@
 ; Example of calling machine language routine from BASIC.
 ; This example takes an integer value passed to it from BASIC,
 ; multiplies it by 2, and returns the result.
-; Code is relocatable, but here use origin of $2EE0 (12000 decimal)
+; Code is relocatable, but here use origin of $7530 (30000 decimal)
 ; as that is what is used in the example.
 ; For more background see chapter 34 of "Getting Started With Extended Color Basic".
 ; Will run standalone as the BASIC program ex4.bas
@@ -16,7 +16,7 @@
 INTCNV  EQU     $DB74           ; Gets integer passed in USR() function and returns in D
 GIVABF  EQU     $DC7B           ; Takes value in D and returns value to BASIC
 
-        ORG     $2EE0           ; Decimal 12000
+        ORG     $7530           ; Decimal 30000
 
         JSR     INTCNV          ; Get value passed in basic
         ASLB                    ; 16-bit shift of D: Rotate B, MSB into Carry
