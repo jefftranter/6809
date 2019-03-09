468c526dd12c5325ffad46242cf55732c4654854
Jeff Tranter <tranter@pobox.com>
2019-03-09 12:43:33 -0500

    Make example better for testing monitor program.

--- sbc/examples/ex1.asm
+++ sbc/examples/ex1.asm
@@ -2,12 +2,16 @@
 ; Does not do anything meaningful, but can be used for testing file
 ; loading and running from ASSIST09.
 
-        ORG     $7000           ; Start address
+        ORG     $1000           ; Start address
 
-START:  LDA     #$12
-        LDB     #$34
-        LDX     #$5678
-        EXG     A,B
+START:  LDA     #$00
+        TFR     A,DP
+        LDA     #$01
+        LDB     #$02
+        LDX     #$0304
+        LDY     #$0506
+        LDU     #$0708
+        ANDCC   #$00
         NOP
         NOP
         RTS
