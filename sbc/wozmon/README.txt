6f6ae940a767de1e8f1a27555109d51de895adbc
Jeff Tranter <tranter@pobox.com>
2019-03-10 19:15:16 -0400

    First version, partially working.

--- sbc/wozmon/README.txt
+++ sbc/wozmon/README.txt
@@ -1,6 +1,2 @@
-Just for fun, this is a port of the monitor program for the Apple 1
-(AKA Woz Mon written by Steve Wozniak) ported to the 6809 processor
-and my single board computer.
-
-It is very simple, but the original program had to fit in 256 bytes of
-ROM.
+This is a port of the 6800 version of Woz Mon to my 6809-based Single
+Board Computer.
--- sbc/wozmon/wozmon.asm
+++ sbc/wozmon/wozmon.asm
@@ -1,176 +1,223 @@
-; Port of Woz Monitor to the 6809.
-; Jeff Tranter <tranter@pobox.com>
-;
+; This is a port of the 6800 version of Woz Mon to my 6809-based Single
+; Board Computer. It was converted to 6809 instructions as well as
+; ported to use the 6850 ACIA for input/output.
+
 ;***********************************************************************
 
-;  The WOZ Monitor for the Apple 1
-;  Written by Steve Wozniak in 1976
+; This is a rewrite of the Apple 1 monitor to run on an MC6800
+; microprocessor, rather than the MCS6502 microprocessor that
+; was standard.  This source code will assemble with the
+; AS Macro Assembler; with minor changes it should assemble
+; with any MC6800 assembler.
 
-; Variables
+; Copyright 2011 Eric Smith <eric@brouhaha.com>
+;
+; This program is free software; you can redistribute and/or modify it
+; under the terms of the GNU General Public License version 3 as
+; published by the Free Software Foundation.
+;
+; This program is distributed in the hope that it will be useful, but
+; WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+; General Public License for more details.
+;
+; The text of the license may be found online at:
+;     http://www.brouhaha.com/~eric/software/GPLv3
+; or:
+;     http://www.gnu.org/licenses/gpl-3.0.txt
 
-        ORG     $1000
+cr          equ  $0d        ; Carriage return
+lf          equ  $0a        ; Line feed
+esc         equ  $1b        ; Escape
 
-XAML    RMB     1               ; Last "opened" location Low
-XAMH    RMB     1               ; Last "opened" location High
-STL     RMB     1               ; Store address Low
-STH     RMB     1               ; Store address High
-L       RMB     1               ; Hex value parsing Low
-H       RMB     1               ; Hex value parsing High
-YSAV    RMB     1               ; Used to see if hex value is given
-MODE    RMB     1               ; $00=XAM, $7F=STOR, $AE=BLOCK XAM
+xam         equ  $0024      ; two bytes
+st          equ  $0026      ; two bytes
+h           equ  $0028
+l           equ  $0029
 
-IN      RMB     128             ; Input buffer
+mode        equ  $002b
+ysav        equ  $002c      ; two bytes
+inptr       equ  $002e      ; two bytes
 
-; Constants
+in          equ  $0200
 
-ACIAC   EQU     $A000           ; 6850 ACIA status/control register
-ACIAD   EQU     $A001           ; 6850 ACIA data register
+aciac       equ  $a000      ; 6850 ACIA status/control register
+aciad       equ  $a001      ; 6850 ACIA data register
 
-; Program
+;           org  $ff00
+            org  $1000
 
-        ORG     $2000           ; To run from RAM
-;       ORG     $FF00           ; To run from ROM
+reset:      lda  #3         ; Reset ACIA
+            sta  aciac
+            lda  #$15       ; Control register setting
+            sta  aciac      ; Initialize ACIA to 8 bits and no parity
 
-RESET   ORCC    #$10            ; SEI, mask interrupts
-        LDS     #$3000          ; Initialize stack
-        LDA     #3              ; Reset ACIA
-        STA     ACIAC
-        LDA     #$15            ; Control register setting
-        STA     ACIAC           ; Initialize ACIA to 8 bits and no parity
+            lds  #$01ff     ; On the 6502, the monitor didn't initialize the
+                            ;  stack pointer, which was OK because it was
+                            ;  guaranteed to be somewhere in page 1. Not so
+                            ;  on the 6800!
+                            ; Ideally, I'd take advantage of the stack
+                            ;  starting right before the input buffer to
+                            ;  save a few bytes, but I haven't yet figured
+                            ;  out how to do it.
 
-NOTCR   CMPA    #'_'            ; Was key "_" ?
-        BEQ     BACKSPACE       ; Yes.
-        CMPA    #$1B            ; ESC?
-        BEQ     ESCAPE          ; Yes.
-        LEAY    1,Y             ; Advance text index.
-        BPL     NEXTCHAR        ; Auto ESC if > 127.
-ESCAPE  LDA     #'\'            ; "\".
-        JSR     ECHO            ; Output it.
-GETLINE LDA     #$0D            ; CR.
-        JSR     ECHO            ; Output it.
-        LDY     #$01            ; Initialize text index.
-BACKSPACE
-        LEAY    -1,Y            ; Back up text index.
-;        BEQ     GETLINE         ; Beyond start of line, reinitialize.
-NEXTCHAR
-        LDA     ACIAC           : Read ACIA
-        BITA    #$01            ; Key ready?
-        BEQ     NEXTCHAR        ; Loop until ready.
-        LDA     ACIAD           ; Load character.
-        STA     IN,Y            ; Add to text buffer.
-        JSR     ECHO            ; Display character.
-        CMPA    #$0D            ; CR?
-        BNE     NOTCR           ; No.
-        LDY     #$FF            ; Reset text index.
-        LDA     #$00            ; For XAM mode.
-        LDX     #0              ; 0->X.
-SETSTOR ASLA                    ; Leaves $7B if setting STOR mode.
-SETMODE STA     MODE            ; $00=XAM $7B=STOR $AE=BLOK XAM
-BLSKIP  LEAY    1,Y             ; Advance text index.
-NEXTITEM
-        LDA     IN,Y            ; Get character.
-        CMPA    #$0D            ; CR?
-        BEQ     GETLINE         ; Yes, done this line.
-        CMPA    #'.'            ; "."?
-        BCS     BLSKIP          ; Skip delimiter.
-        BEQ     SETMODE         ; Yes. Set STOR mode.
-        CMPA    #':'            ; ":"?
-        BEQ     SETSTOR         ; Yes. Set STOR mode.
-        CMPA    #'R'            ; "R"?
-        BEQ     RUN             ; Yes. Run user program.
-        STX     L               ; $00-> L.
-        STX     H               ; and H.
-        STY     YSAV            ; Save Y for comparison.
-NEXTHEX LDA     IN,Y            ; Get character for hex test.
-        EORA    #$B0            ; Map digits to $0-9.
-        CMPA    #$0A            ; Digit?
-        BCS     DIG             ; Yes.
-        ADCA    #$88            ; Map letter "A"-"F" to $FA-FF.
-        CMPA    #$FA            ; Hex letter?
-        BCS     NOTHEX          ; No, character not hex.
-DIG     ASLA
-        ASLA                    ; Hex digit to MSD of A.
-        ASLA
-        ASLA
-        LDX     #$04            ; Shift count.
-HEXSHIFT
-        ASL                     ; Hex digit left, MSB to carry.
-        ROL     L               ; Rotate into LSD.
-        ROL     H               ;  Rotate into MSD’s.
-        LEAX    -1,X            ; Done 4 shifts?
-        BNE     HEXSHIFT        ; No, loop.
-        LEAY    1,Y             ; Advance text index.
-        BNE     NEXTHEX         ; Always taken. Check next char for hex.
-NOTHEX  CMPY    YSAV            ; Check if L, H empty (no hex digits).
-        LBEQ    ESCAPE          ; Yes, generate ESC sequence.
-        BITA    MODE            ; Test MODE byte.
-        BVC     NOTSTOR         ;  B6=0 STOR 1 for XAM & BLOCK XAM
-        LDA     L               ; LSD’s of hex data.
-        STA     [STL,X]         ; Store at current ‘store index’.
-        INC     STL             ; Increment store index.
-        BNE     NEXTITEM        ; Get next item. (no carry).
-        INC     STH             ; Add carry to ‘store index’ high order.
-TONEXTITEM
-        JMP     NEXTITEM        ; Get next command item.
-RUN     JMP     [XAML]          ; Run at current XAM index.
-NOTSTOR BMI     XAMNEXT         ; B7=0 for XAM, 1 for BLOCK XAM.
-        LDX     #$02            ; Byte count.
-SETADR  LDA     L-1,X           ; Copy hex data to
-        STA     STL-1,X         ; ‘store index’.
-        STA     XAML-1,X        ; And to ‘XAM index’.
-        LEAX    -1,X            ; Next of 2 bytes.
-        BNE     SETADR          ; Loop unless X=0.
-NXTPRNT BNE     PRDATA          ; NE means no address to print.
-        LDA     #$8D            ; CR.
-        JSR     ECHO            ; Output it.
-        LDA     XAMH            ; ‘Examine index’ high-order byte.
-        JSR     PRBYTE          ; Output it in hex format.
-        LDA     XAML            ; Low-order ‘examine index’ byte.
-        JSR     PRBYTE          ; Output it in hex format.
-        LDA     #':'            ; ":".
-        JSR     ECHO            ; Output it.
-PRDATA  LDA     #$A0            ; Blank.
-        JSR     ECHO            ; Output it.
-        LDA     [XAML,X]        ; Get data byte at ‘examine index’.
-        JSR     PRBYTE          ; Output it in hex format.
-XAMNEXT STX     MODE            ; 0->MODE (XAM mode).
-        LDA     XAML
-        CMPA    L               ; Compare ‘examine index’ to hex data.
-        LDA     XAMH
-        SBCA    H
-        BCC     TONEXTITEM      ; Not less, so no more data to output.
-        INC     XAML
-        BNE     MOD8CHK         ; Increment ‘examine index’.
-        INC     XAMH
-MOD8CHK LDA     XAML            ; Check low-order ‘examine index’ byte
-        ANDA    #$07            ; For MOD 8=0
-        BPL     NXTPRNT         ; Always taken.
-PRBYTE  PSHS    A               ; Save A for LSD.
-        LSRA
-        LSRA
-        LSRA                    ; MSD to LSD position.
-        LSRA
-        JSR     PRHEX           ; Output hex digit.
-        PULS    A               ; Restore A.
-PRHEX   ANDA    #$0F            ; Mask LSD for hex print.
-        ORA     #'0'            ; Add "0".
-        CMPA    #$3A            ; Digit?
-        BCS     ECHO            ; Yes, output it.
-        ADDA    #$07            ; Add offset for letter.
-ECHO    LDB     ACIAC
-        BITB    #$02            ; bit (B2) cleared yet?
-        BEQ     ECHO            ; No, wait for display.
-        STA     ACIAD           ; Output character. Sets DA.
-        RTS                     ; Return.
+            bra  escape
 
-; Interrupt Vectors - all point to monitor start for now.
-; Comment out when running from RAM.
-;       ORG     $FF10
-        FDB     RESET           ; Reserved vector
-        FDB     RESET           ; SWI3 vector
-        FDB     RESET           ; SWI2 vector
-        FDB     RESET           ; FIRQ vector
-        FDB     RESET           ; IRQ vector
-        FDB     RESET           ; SWI vector
-        FDB     RESET           ; NMI vector
-        FDB     RESET           ; Reset vector
+; Get a line of input from the keyboard, echoing to display.
+; Normally enter at escape or getline.
+
+notcr:      cmpa #'_'       ; "_"?  [NB back arrow]
+            beq  backspace  ; Yes.
+            cmpa #esc       ; ESC?
+            beq  escape     ; Yes.
+            leax 1,x        ; Advance text index.
+            incb
+            bpl  nextchar   ; Auto ESC if > 127.
+
+escape:     lda  #'\'       ; "\".
+            jsr  echo       ; Output it.
+
+getline:    lda  #cr        ; CR.
+            jsr  echo       ; Output it.
+            lda  #lf
+            jsr  echo       ; Output it.
+            ldx  #in+1      ; Initiallize [sic] text index.
+            ldb  #1
+backspace:  leax -1,x       ; Back up text index.
+            decb
+            bmi  getline    ; Beyond start of line, reinitialize.
+
+nextchar:   lda  aciac      ; Key ready?
+            bita #$01
+            beq  nextchar   ; Loop until ready.
+            lda  aciad      ; Load character.
+            sta  ,x         ; Add to text buffer.
+            bsr  echo       ; Display character.
+            cmpa #cr        ; CR?
+            bne  notcr      ; No.
+
+; Process an input line.
+
+ret:        ldx  #in+256-1  ; Reset text index to in-1, +256 so that
+                            ;  'inc inptr+1' will result in $0200.
+            stx  inptr
+            clra            ; For XAM mode. 0->B.
+
+setblok:    asla            ; Leaves $56 if setting BLOCK XAM mode.
+setmode:    sta  mode       ; $00 = XAM, $BA = STOR, $56 = BLOK XAM.
+blskip:     inc  inptr+1    ; Advance text index.
+nextitem:   ldx  inptr
+            lda  ,x         ; Get character.
+            cmpa #cr        ; CR?
+            beq  getline    ; Yes, done this line.
+            cmpa #'.'       ; "."?
+            beq  setblok    ; Set BLOCK XAM mode.
+            bls  blskip     ; Skip delimiter.
+            cmpa #':'       ; ":"?
+            beq  setmode    ; Yes, set STOR mode.
+            cmpa #'R'       ; "R"?
+            beq  run        ; Yes, run user program.
+            clr  l          ; $00->L.
+            clr  h          ;  and H.
+            stx  ysav       ; Save Y for comparison.
+
+nexthex:    ldx  inptr
+            lda  ,x         ; Get character for hex test.
+            eora #'0'       ; Map digits to $0-9.
+            cmpa #$09       ; Digit?
+            bls  dig        ; Yes.
+            adda #$89       ; Map letter "A"-"F" to $FA-FF.
+            cmpa #$f9       ; Hex letter?
+            bls  nothex     ; No, character not hex.
+
+dig:        asla            ; Hex digit to MSD of A.
+            asla
+            asla
+            asla
+
+            ldb  #$04       ; Shift count.
+hexshift:   asla            ; Hex digit left, MSB to carry.
+            rol  l          ; Rotate into LSD.
+            rol  h          ; Rotate into MSD's.
+            decb            ; Done 4 shifts?
+            bne  hexshift   ; No, loop.
+
+            inc  inptr+1    ; Advance text index.
+            bra  nexthex    ; Always taken. Check next character for hex.
+
+nothex:     cpx  ysav       ; Check if L, H empty (no hex digits).
+            beq  escape     ; Yes, generate ESC sequence.
+            tst  mode       ; Test MODE byte.
+            bpl  notstor    ; B6=0 for STOR, 1 for XAM and BLOCK XAM
+
+; STOR mode
+            ldx  st
+            lda  l          ; LSD's of hex data.
+            sta  ,x         ; Store at current 'store index'.
+            leax 1,x
+            stx  st
+tonextitem: bra  nextitem   ; Get next command item.
+
+prbyte:     pshs a          ; Save A for LSD.
+            lsra
+            lsra
+            lsra            ; MSD to LSD position.
+            lsra
+            bsr  prhex      ; Output hex digit.
+            puls a          ; Restore A.
+prhex:      anda #$0f       ; Mask LSD for hex print.
+            ora  #'0'       ; Add "0".
+            cmpa #'9'       ; Digit?
+            bls  echo       ; Yes, output it.
+            adda #$07       ; Add offset for letter.
+
+echo:       ldb  aciac
+            bitb #$02       ; bit (B2) cleared yet?
+            beq  echo       ; No, wait for display.
+            sta  aciad      ; Output character.
+            rts             ; Return.
+
+run:        ldx  xam
+            jmp  ,x         ; Run at current XAM index.
+
+notstor:    bne  xamnext    ; mode = $00 for XAM, $56 for BLOCK XAM.
+
+            ldx  h          ; Copy hex data to
+            stx  st         ;  'store index'.
+            stx  xam        ; And to 'XAM index'.
+            clra            ; set Z flag to force following branch.
+
+nxtprnt:    bne  prdata     ; NE means no address to print.
+            lda  #cr        ; CR.
+            bsr  echo       ; Output it.
+            lda  #lf        ; LF.
+            bsr  echo       ; Output it.
+            lda  xam        ; 'Examine index' high-order byte.
+            bsr  prbyte     ; Output it in hex format.
+            lda  xam+1      ; Low-order 'Examine index' byte.
+            bsr  prbyte     ; Output it in hex format.
+            lda  #':'       ; ":".
+            bsr  echo       ; Output it.
+
+prdata:     lda  #' '       ; Blank.
+            bsr  echo       ; Output it.
+
+            ldx  xam
+            lda  ,x         ; Get data byte at 'examine index'.
+            bsr  prbyte     ; Output it in hex format.
+
+xamnext:    clr  mode       ; 0->MODE (XAM mode).
+            ldx  xam        ; Compare 'examine index' to hex data.
+            cpx  h
+            beq  tonextitem ; Not less, so more data to output.
+            leax 1,x
+            stx  xam
+            lda  xam+1      ; Check low-order 'examine index' byte
+            anda #$07       ;  For MOD 8 = 0
+            bra  nxtprnt    ; always taken
+
+            org  $fff8      ; vector table
+            fdb  $0000      ; IRQ
+            fdb  $0000      ; SWI
+            fdb  $f000      ; NMI
+            fdb  $ff00      ; RESET
